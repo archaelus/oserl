73ff6e23831879f1398202f6b18b5a84fd2f7de9
mpquique <mpquique>
2005-04-08 00:26:47 +0000

    - Functions set_udhi/1 and split_user_data/2 added.

--- src/sm.erl
+++ src/sm.erl
@@ -45,6 +45,14 @@
 %%%   </li>
 %%% </ul>
 %%%
+%%% [7 Abr 2005]
+%%%
+%%% <ul>
+%%%   <li>Functions <a href="#set_udhi-1">set_udhi/1</a> and 
+%%%     <a href="#split_user_data-2">split_user_data/2</a> added.
+%%%   </li>
+%%% </ul>
+%%%
 %%%
 %%% @copyright 2003 - 2004 Enrique Marcote Peña
 %%% @author Enrique Marcote Peña <mpquique_at_users.sourceforge.net>
@@ -56,6 +64,7 @@
 %%%-------------------------------------------------------------------
 %%% Include files
 %%%-------------------------------------------------------------------
+-include("smpp_base.hrl").
 
 %%%-------------------------------------------------------------------
 %%% External exports
@@ -63,7 +72,9 @@
 -export([message_user_data/1, 
          reply_addresses/1,
          reply_destination_address/1,
-         reply_source_address/1]).
+         reply_source_address/1,
+         set_udhi/1,
+         split_user_data/2]).
 
 %%%-------------------------------------------------------------------
 %%% Undocumented exports
@@ -78,6 +89,10 @@
 %%%-------------------------------------------------------------------
 %%% Macros
 %%%-------------------------------------------------------------------
+-define(UDHL, 5).  % UDH Length excluded length for Concatenated SMs
+-define(IEI, 0).   % Information Element Identifier for Concatenated SMs
+-define(IEIDL, 3). % IEI Data length excluded length
+-define(REF_NUM(N), N rem 256).
 
 %%%-------------------------------------------------------------------
 %%% Records
@@ -153,6 +168,58 @@ reply_source_address(Pdu) ->
      {source_addr, operation:get_param(destination_addr, Pdu)}].
 
 
+%% @spec set_udhi(ParamList) -> NewParamList
+%%    ParamList  = [{ParamName, ParamValue}]
+%%    ParamName  = atom()
+%%    ParamValue = term()
+%%
+%% @doc Sets UDHI in the <i>esm_class</i> parameter of given list.  If
+%% <i>esm_class</i> was not defined, returned list includes it with a
+%% value of <tt>ESM_CLASS_GSM_UDHI</tt>.
+%% @end 
+set_udhi(ParamList) ->
+    case lists:keysearch(esm_class, 1, ParamList) of
+        {value, {esm_class, EsmClass}} ->
+            NewEsmClass = EsmClass bor ?ESM_CLASS_GSM_UDHI,
+            lists:keyreplace(esm_class, 1, ParamList, {esm_class,NewEsmClass});
+        false ->
+            [{esm_class, ?ESM_CLASS_GSM_UDHI}|ParamList]
+    end.
+
+
+%% @spec split_user_data(Data, RefNum) -> Segments
+%%    Data = string()
+%%    RefNum = int()
+%%    Segments = [Segment]
+%%    Segment = string()
+%%
+%% @doc Splits the user data into several segments for message concatenation.
+%%
+%% <p><tt>Data</tt> is only splited if longer than SM_MAX_SIZE macro
+%% (defaults to 160), otherwise the list <tt>[Data]</tt> is returned.</p>
+%%
+%% <p>Resulting <tt>Segments</tt> contain the appropriated UDH for message
+%% concatenation, thus they are at most SM_SEGMENT_MAX_SIZE + UDH length (6).
+%% </p>
+%%
+%% <p><tt>Segments</tt> are returned in the right order.</p>
+%% @end 
+split_user_data(Data, RefNum) when length(Data) > ?SM_MAX_SIZE ->
+    split_user_data(Data, ?REF_NUM(RefNum), []);
+split_user_data(Data, _RefNum) ->
+    [Data].
+
+%% @doc Auxiliary function for split_user_data/2
+%%
+%% @see split_user_data/2
+%% @end 
+split_user_data(Data, RefNum, Acc) when length(Data) > ?SM_SEGMENT_MAX_SIZE ->
+    {Segment, Rest} = lists:split(?SM_SEGMENT_MAX_SIZE, Data),
+    split_user_data(Rest, RefNum, [Segment|Acc]);
+split_user_data(Data, RefNum, Acc) ->
+    append_udh([Data|Acc], RefNum).
+
+
 %%%===================================================================
 %%% Undocumented functions
 %%%
@@ -167,3 +234,25 @@ reply_address(Pdu) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+%% @spec append_udh(Segments, RefNum) -> UdhSegments
+%%
+%% @doc <tt>Segments</tt> are assumed to be in reversed order.
+%%
+%% @see split_user_data/2
+%% @end 
+append_udh(Segments, RefNum) ->
+    TotalSegments = length(Segments),
+    append_udh(Segments, RefNum, TotalSegments, TotalSegments, []).
+
+%% @doc Auxiliary function for append_udh/2
+%%
+%% @see append_udh/2
+%% @end 
+append_udh([], _RefNum, _TotalSegments, _SeqNum, Acc) ->
+    Acc;
+append_udh([H|T], RefNum, TotalSegments, SeqNum, Acc) ->
+    % UDH: UDHL, IEI, IEIDL, RefNum, TotalSegments, SeqNum
+    NewH = [?UDHL, ?IEI, ?IEIDL, RefNum, TotalSegments, SeqNum | H],
+    append_udh(T, RefNum, TotalSegments, SeqNum - 1, [NewH|Acc]).
+
+

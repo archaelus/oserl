59f6171ac37efc30574f66a1dbd91afb7883c632
mpquique <mpquique>
2006-08-22 17:31:54 +0000

    move version number to 1.3

--- common.mk
+++ common.mk
@@ -2,7 +2,7 @@
 # Common Macros
 # ----------------------------------------------------
 APPNAME = oserl
-VSN = 1.2
+VSN = 1.3
 
 # FIXME These variables should be automatically set when compiling
 ERL = erl
@@ -18,9 +18,9 @@ INSTALL_DATA = ${INSTALL} -m 644
 # System dependent paths
 # ----------------------------------------------------
 ### oserl use eDoc for documentation, to regenerate update paths as needed!
-EDOC_APP = /usr/local/lib/erlang/lib/edoc-0.4
-XMERL_APP = /usr/local/lib/erlang/lib/xmerl-0.19
-SYNTAX_TOOLS_APP = /usr/local/lib/erlang/lib/syntax_tools-1.3
+EDOC_APP = /usr/local/lib/erlang/lib/edoc-0.6.9
+XMERL_APP = /usr/local/lib/erlang/lib/xmerl-1.0.5
+SYNTAX_TOOLS_APP = /usr/local/lib/erlang/lib/syntax_tools-1.5.0
 
 EDOC_PATHS = \
 	-pa $(EDOC_APP)/ebin -pa $(XMERL_APP)/ebin -pa $(SYNTAX_TOOLS_APP)/ebin
